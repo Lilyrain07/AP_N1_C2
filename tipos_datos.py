1130fd5e8d4c378798053a711b45bf1620e23de1
Lilyrain07 <ruthelisa0717@gmail.com>
2026-03-31 10:53:45 -0300

    primera carga de codigo

---
+++ tipos_datos.py
@@ -0,0 +1,14 @@
+#Tipos de datos en pyton
+
+numero_real = 0 
+numero_decimal = 0.0
+cadena_texto = "quiero irme a dormir!"
+
+
+print("mi primer mensaje en python")
+print(numero_real)
+print(type(numero_real))
+print(numero_decimal)
+print(type(numero_decimal))
+print((cadena_texto))
+print(type(cadena_texto))
