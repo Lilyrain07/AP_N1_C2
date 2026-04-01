abd8921a40c0fb1a834328a3bcadbc40c7a3bad7
Lilyrain07 <ruthelisa0717@gmail.com>
2026-04-01 10:53:46 -0300

    clase 1 abril

--- tipos_datos.py
+++ tipos_datos.py
@@ -1,14 +1,60 @@
 #Tipos de datos en pyton
 
-numero_real = 0 
+print(type(5))
+print(type(3.2))
+print(type("hola"))
+print(type(True))
+
+#la variable numero_real almacenara un dato de numero entero
+numero_entero = 0 
 numero_decimal = 0.0
 cadena_texto = "quiero irme a dormir!"
-
+valor_booleano = False
 
 print("mi primer mensaje en python")
-print(numero_real)
-print(type(numero_real))
+
+print()
+print(numero_entero)
+print(type(numero_entero))
+
+print()
 print(numero_decimal)
 print(type(numero_decimal))
+
+print()
 print((cadena_texto))
 print(type(cadena_texto))
+
+print()
+print(valor_booleano)
+print(type(valor_booleano))
+
+valor_booleano= 5 > 3
+print()
+print(valor_booleano)
+print(type(valor_booleano))
+
+#CONCATENACION de cadenas de texto
+saludo = "buen dia querida "
+nombre = "elisa arias"
+print
+print(saludo + nombre)
+
+texto_de_una_linea_comillas_simples = 'texto de una linea'
+texto_de_una_linea_comillas_dobles = "texto de una linea"
+
+texto_de_multiples_lineas_comillas_simples = '''
+linea 1 de texto de una linea
+linea 2 de texto de una linea
+linea 3 de tecto de una linea
+'''
+
+texto_de_multiples_lineas_comillas__dobles = """
+linea 1 de texto de una linea
+linea 2 de texto de una linea
+linea 3 de tecto de una linea
+"""
+
+
+print(texto_de_multiples_lineas_comillas__dobles)
+
---
+++ variables.py
@@ -0,0 +1,17 @@
+numero_1= 0
+numero_2 = 0
+resultado = 0
+
+numero_1 = int(input('ingrese su numero: '))
+print(numero_1)
+
+numero_2 = int(input('ingrese numero 2: '))
+print(numero_2)
+
+resultado = numero_1 + numero_2
+
+print('su resultado es ' + str(resultado))
+
+
+
+
