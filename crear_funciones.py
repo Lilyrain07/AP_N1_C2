3230229563ffef582c51a8330712866a0cff8de7
Lilyrain07 <ruthelisa0717@gmail.com>
2026-04-29 10:43:22 -0400

    29/04

--- crear_funciones.py
+++ crear_funciones.py
@@ -20,12 +20,19 @@ def division(num_1,num_2):
         print(f'{num_1} / {num_2} = {resultado}')
     else:
          print ('no se puede dividir por 0')
-
+'''
 
 def pedir_datos():
-    num_1= input('ingrese primer numero')
-    num_2= input('ingrese segundo numero')
+    str_num_1= input('ingrese primer numero: ')
+    str_num_2= input('ingrese segundo numero: ')
 
+    num_1 = convertir_float(str_num_1)
+    num_2 = convertir_float(str_num_2)
+
+    if num_1 and num_2 != False:
+        return num_1 and num_2
+    else:
+        return (False,False) 
 
     if num_1.isdigit() and num_2.isdigit():
 
@@ -35,19 +42,36 @@ def pedir_datos():
         print('valor NO corresponde a un numero')
     return(num_a,num_b)
 
-    
+def convertir_float(valor):
+    try:
+        return float(valor)
+    except (ValueError, TypeError):
+        return False
 
-#suma(num_1,num_2)
-#resta(num_1,num_2)
-#multiplicacion(num_1,num_2)
-#division(num_1,num_2)
 
-
-'''
-
-print (' BIENVENIDO A MI PRIMERA CALCULADORA')
-print('===========================')
+print(' BIENVENIDO A MI PRIMERA CALCULADORA')
+print('                                   ')
 ciclo = True
+
+def resta(a,b):
+    resultado= a-b
+    print (f'{a}-{b}= {resultado}')
+
+def suma (a,b):
+    resultado = a+b
+    print (f'{a}+ {b} = {resultado}')
+
+def multiplicacion(a,b):
+    resultado = a*b
+    print (f'{a}*{b} = {resultado}')
+
+def division(a,b):
+    if b != 0:
+        resultado = a/b
+        print (f'{a}/{b} = {resultado}')
+    else:
+        print('DIVISOR INGRESADO:0! no se puede dividir por ese numero')
+
 
 while ciclo == True:
     print('[1] suma')
@@ -57,22 +81,28 @@ while ciclo == True:
     print('[0] salir') 
     opcion= input( 'seleccione su operacion [0-4]: ')
 
-
-
-if opcion == '0':
-    ciclo= False
-    print ('gracias por usar esta calculadora')
-    print ('saliendo...')
-else:
-    a,b = pedir_datos()
-
-    if opcion == '1':
-        suma(a,b)
-    elif opcion == '2':
-        resta(a,b)
-    elif opcion =='3':
-        multiplicacion(a,b)
-    elif opcion == '4':
-        division(a,b)
+    if opcion == '0':
+        ciclo= False
+        print ('gracias por usar esta calculadora')
+        print ('saliendo...')
     else:
-     print('opcion no valida')
+        a,b = pedir_datos()
+
+        if opcion == '1':
+            suma (a,b)
+
+        elif opcion == '2':
+            resta(a,b)
+            
+        elif opcion =='3':
+            multiplicacion(a,b)
+            
+        elif opcion == '4':
+            division(a,b)
+    
+        else:
+            print('opcion no valida')
+
+        #def es sacronimom de definir porque estamos definiendo una funcion
+        #luego de def ponemos el nombre de la funcion
+        #luego del nombre, ponemos sus argumentos
---
+++ ejercicios.py
@@ -0,0 +1,52 @@
+# 1._ escriba unafuncion que calcule el total de una factura tras aplicarle el IVA.
+#    la funcion debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar,
+# si la funcion no recibe el porcentaje del IVA, debera aplicar por defecto el 19% 
+
+# 2._ Escriba una funcion que calcule el area de un circulo y otra que calcule 
+# el volumen de un cilindro usando la primera funcion del area
+import math
+import calculadora
+
+def area_circunferencia(radio):
+    pi = math.pi
+    resultado = pi * radio *radio
+    return resultado
+
+def volumen_cilindro():
+area = area_circunferencia(radio)
+resultado = area *altura
+return resultado
+
+def calculo_volumen_cilindro():
+    print('ingrese los datos solicitados')
+    str_radio= input('radio de cilindro: ')
+    str_altura = input ('altura de cilindro: ')
+    radio = calculadora.convertir_float (str_radio)
+    altura = calculadora.convertir_float(str_altura)
+    volumen = volumen_cilindro(radio, altura)
+
+#3._ escriba una funcion que permita escribir la tabla de multiplicar de un numero ingresado por el usuario
+
+# tablas de multiplicar
+
+while True:
+    print('[1] calculo de IVA')
+    print('[2] calculo de volumen cilindro')
+    print('[3] tabla de multiplicar')
+    print('[0] salir')
+
+    opcion =input ('ingrese su opcion [0-3]')
+    valores_opcion = range (4)
+
+    if opcion in valores_opcion:
+        if opcion =='1':
+            pass
+        elif opcion =='2':
+            pass
+        elif opcion =='3':
+            pass
+        elif opcion == '0':
+            print ('saliendo del sistemaa')
+            break
+    else:
+        print('opcion seleccionada no valida')
