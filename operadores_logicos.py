f2da0ca99f9adbae77e09abf071063ddb545f295
Lilyrain07 <ruthelisa0717@gmail.com>
2026-05-05 10:56:39 -0400

    5/5

---
+++ operadores_logicos.py
@@ -0,0 +1,12 @@
+print('\noperador logico OR')
+print(f'V o V = {True or True}')
+print(f'V o F = {True or False}')
+print(f'F o V = {False or True}')
+print(f'F o F = {False or False}')
+
+print('\noperador logico Y (and)')
+
+print(f'V and V = {True and True}')
+print(f'V and F = {True and False}')
+print(f'F and V = {False and True}')
+print(f'F and F = {False and False}')
