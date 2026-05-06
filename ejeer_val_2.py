d2ed3b3be52f0276042b200553a6049a361b8909
Lilyrain07 <ruthelisa0717@gmail.com>
2026-05-06 10:02:10 -0400

    6/5

---
+++ ejeer_val_2.py
@@ -0,0 +1,11 @@
+#los ejercicios a resoolver deben incluirse dentro de un menu
+#mediante el cual se ejecutara cada uno de ellos
+
+#1.- escriba una funcion que al ser llamada imprima el saludo "buen dia"
+
+#2.-escruba una funcion que solicite al usuario ingresar su nombre
+#en unba variable nombre_usuario
+
+#3.- escribir una funcion que pida al usuario un numero entero menor a 10
+#y al ser llamada entregue el factorial de ese numero
+
