eb9219d8023538cc6036aa11ed7f5987d1040ee0
Lilyrain07 <ruthelisa0717@gmail.com>
2026-05-06 10:41:56 -0400

    6/5

--- ejeer_val_2.py
+++ ejeer_val_2.py
@@ -8,4 +8,38 @@
 
 #3.- escribir una funcion que pida al usuario un numero entero menor a 10
 #y al ser llamada entregue el factorial de ese numero
+import math
 
+def menu_principal():
+    while True:
+        print()
+        print('[1] saludo')
+        print('[2] nombre')
+        print('[3] factorial')
+        print('[0] Salir')
+
+        opcion = input('Ingrese su Opción [0-3]: ')
+        valores_opcion = ['0','1','2','3']
+
+        if opcion in valores_opcion:
+            if opcion == '1':
+                saludo = ('¡Buenos dias estudiante inacap!')
+                print(saludo)
+            elif opcion == '2':
+                nombre_usuario = input('¿cual es su nombre?: ')
+                print (f'buenos dias {nombre_usuario} ')
+            elif opcion == '3':
+                numero= input('ingrese el numero que quiere (debe ser un numero MENOR A 10!): ')
+                if numero < 10:
+                    resultado = math.factorial(numero)
+                    print (f'el resultado de su numero en factoriales es: {numero}! = {resultado} ')
+                else:
+                    print('numero ingresado no corresponde, intentelo de nuevo.')
+
+            elif opcion == '0':
+                print('Saliendo del sistema...')
+                break
+        else:
+            print('Opción ingresada NO corresponde...')
+
+menu_principal()
--- ejercicios.py
+++ ejercicios.py
@@ -1,54 +1,92 @@
-# 1._ escriba unafuncion que calcule el total de una factura tras aplicarle el IVA.
-#    la funcion debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar,
-# si la funcion no recibe el porcentaje del IVA, debera aplicar por defecto el 19% 
+from math import pi as pi
 
-# 2._ Escriba una funcion que calcule el area de un circulo y otra que calcule 
-# el volumen de un cilindro usando la primera funcion del area
-import math
-import calculadora
-from calculadora import convertir_float
+# 1.- Escriba una función que calcule el total de una factura tras aplicarle el IVA.
+#       La función debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar,
+#       si la función no recibe el porcentaje de IVA, deberá aplicar por defecto un 19%
+def pedir_datos_factura():
+    str_monto = input('Ingrese el monto de la factura: ')
+    str_procentaje = input('Ingrese el pordentaje de IVA\n(o deje en blanco para aplicar 19%): ')
+    if str_procentaje == '':
+        str_procentaje = '19'
+    monto = convertir_float(str_monto)
+    porcentaje = convertir_float(str_procentaje)
+    factura(monto,porcentaje)
+
+def factura(monto,porcentaje):
+    if monto and porcentaje != 0:
+        monto_iva = monto * (porcentaje/100)
+        total = monto + monto_iva
+        print()
+        print(f'Monto Factura: ${round(monto)}')
+        print(f'Monto IVA: ${round(monto_iva)}')
+        print(f'% IVA aplicado: {round(porcentaje)}')
+        print(f'Total Factura: ${round(total)}')
+    else:
+        print('valores ingresados NO corresponden.')
+# 2.- Escriba una función que calcule el área de un círculo y otra que calcule
+#       el volumen de un cilindro usando la primera función de área.
 
 def area_circunferencia(radio):
-    pi = math.pi
-    resultado = pi * radio *radio
+    resultado = pi * radio * radio
     return resultado
 
-def volumen_cilindro(radio, altura):
+def volumen_cilindro(radio,altura):
     area = area_circunferencia(radio)
-    resultado = area *altura
+    resultado = area * altura
     return resultado
 
 def calculo_volumen_cilindro():
-    print('ingrese los datos solicitados')
-    str_radio= input('radio de cilindro: ')
-    str_altura = input ('altura de cilindro: ')
-    radio = calculadora.convertir_float(str_radio)
-    altura = calculadora.convertir_float(str_altura)
-    volumen = volumen_cilindro(radio, altura)
+    print('Ingrese los datos solicitados')
+    str_radio = input('Radio Cilindro: ')
+    str_altura = input('Altura Cilindro: ')
+    radio = convertir_float(str_radio)
+    altura = convertir_float(str_altura)
+    volumen = volumen_cilindro(radio,altura)
     print(volumen)
 
-#3._ escriba una funcion que permita escribir la tabla de multiplicar de un numero ingresado por el usuario
+# 3.- Escriba una función que permita escribir la tabla de multiplicar de un número 
+# ingresado por el usuario.
 
-# tablas de multiplicar
+def tabla():
+    str_numero = input('Ingrese un número: ')
+    numero = convertir_float(str_numero)
+    factor = 1
+    if numero != None:
+        while factor <= 12:
+            total = numero * factor
+            print(f'{numero} x {factor} = {total}')
+            factor += 1
+
+# Funciones auxiliares
+def convertir_float(valor):
+    try:
+        return float(valor)
+    except ValueError:
+        print('El valor ingresado NO se puede convertir a número.')
+    except Exception as error:
+        print(f'Se ha producido el siguiente error: {error}')
+        print(f'Tipo de error: {type(error)}')
+        print(f'Argumentos del error: {error.args}')
+        return None
 
 while True:
-    print('[1] calculo de IVA')
-    print('[2] calculo de volumen cilindro')
-    print('[3] tabla de multiplicar')
-    print('[0] salir')
+    print('[1] Cálculo IVA')
+    print('[2] Cálculo Volumen Cilindro')
+    print('[3] Tabla de Multiplicar')
+    print('[0] Salir')
 
-    opcion =input ('ingrese su opcion [0-3]')
-    valores_opcion = range (4)
+    opcion = input('Ingrese su Opción [0-3]')
+    valores_opcion = ['0','1','2','3']
 
     if opcion in valores_opcion:
-        if opcion =='1':
+        if opcion == '1':
             pass
-        elif opcion =='2':
-            pass
-        elif opcion =='3':
-            pass
+        elif opcion == '2':
+            calculo_volumen_cilindro()
+        elif opcion == '3':
+            tabla()
         elif opcion == '0':
-            print ('saliendo del sistemaa')
+            print('Saliendo del sistema...')
             break
     else:
-        print('opcion seleccionada no valida')
+        print('Opción ingresada NO corresponde...')
