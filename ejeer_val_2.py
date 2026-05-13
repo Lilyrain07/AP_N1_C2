8f352fe47547ce587407c38a5978ff8efc59f56b
Lilyrain07 <ruthelisa0717@gmail.com>
2026-05-13 10:37:19 -0400

    13 04

--- ejeer_val_2.py
+++ ejeer_val_2.py
@@ -1,49 +1,83 @@
-#los ejercicios a resoolver deben incluirse dentro de un menu
-#mediante el cual se ejecutara cada uno de ellos
+# Los ejercicios a resolver deben incluirse dentro de un menú
+# mediante el cual se ejecutará cada uno de ellos.
 
-#1.- escriba una funcion que al ser llamada imprima el saludo "buen dia"
-def saludo():
-    saludo = '¡Buenos dias estudiante inacap!'
+# 1.- Escriba una función que al ser llamada imprima el saludo "Buen día!"
+def saludo_cordial():
+    saludo = '!Buen día!'
     return saludo
-#2.-escriba una funcion que solicite al usuario ingresar su nombre
-#en unba variable nombre_usuario
 
-def nombre_usuario():
-    nombre_usuario = input('ingrese su nombre: ')
+# 2.- Escriba una función que solicite al usuario ingresar su nombre
+# en una variable nombre_usuario
+# y al ser llamada imprima el saludo "!Buen día 'nombre_usuario'!"
+def saludo_personal():
+    saludo = saludo_cordial()
+    saludo_mod = saludo.split('!')
+    saludo_mod_2 = saludo.replace('!',' ')
+    nombre_usuario = solicitar_nombre_usuario()
+    saludo_1_split = saludo_mod[0] + ' ' + nombre_usuario + '!'
+    saludo_2_split = f'{saludo_mod[0]} {nombre_usuario}!'
+    saludo_1_replace = saludo_mod_2 + nombre_usuario + '!'
+    saludo_2_replace = f'{saludo_mod_2}{nombre_usuario}!'
+    return saludo_1_split + '\n' + saludo_2_split + '\n' + saludo_1_replace + '\n' + saludo_2_replace
+
+def solicitar_nombre_usuario():
+    nombre_usuario = input('Ingrese su nombre: ')
     return nombre_usuario.title()
 
 
-#3.- escribir una funcion que pida al usuario un numero entero menor a 10
-#y al ser llamada entregue el factorial de ese numero
-import math
+# 3.- Escribir una función que pida al usuario un número entero menor a 10
+# y al ser llamada entregue el factorial de ese número
+def factorial():
+    numero = int(input('Ingrese un número entero: '))
+    resultado = 1
+    for valor in range(1,numero + 1):
+        resultado = resultado * valor
+    return f'{numero}! = {resultado}'
+#4.- solicite al usuario que ingrese 3 noras y calcular el promedio indicando si el alumno aprueba 
+#(nota >= 4.0) o reprueba (nota< 4.0)
+
+def promedio():
+    nombre_estudiante =solicitar_nombre_usuario()
+    notas_estudiante = []
+    cantidad_notas = 3
+
+while len(notas_estudiante) <= cantidad_notas:
+    nota = ingreso_notas()
+    if nota:
+        notas_estudiante.append(nota)
+
+
+       
+def ingreso_notas():
+    nota_minima = 1.0
+    nota_maxima = 7.0
+    try:
+        nota = float(input('Ingrese una nota: '))
+        if nota >nota_minima and nota <= nota_maxima:
+            return nota
+        else:
+            print(f'nota debe estar entre {nota_minima} y {nota_maxima}')
+    except ValueError:
+        print('valor ingresado invalido.')
+        return ingreso_notas()
+    except Exception as error:
+        print(f'Error inesperado: {error}')
+        return ingreso_notas()
+
+
 
 def menu_principal():
-    while True:
-        print()
-        print('[1] saludo')
-        print('[2] nombre')
-        print('[3] factorial')
-        print('[0] Salir')
+    diccionario_menu = {
+        '1': 'saludo_cordial',
+        '2': 'saludo_personal',
+        '3': 'factorial',
+        '4': 'promedio',
+        '0': 'Salir'
+    }
+    titulo = 'ejercicios evaluacion 2'
+    print(titulo)
+    print(f'{'=' * len(titulo)}')
+    
+    for clave ,valor in diccionario_menu.items():
+        print ("[' + clave + '] " + valor)
 
-        opcion = input('Ingrese su Opción [0-3]: ')
-        valores_opcion = ['0','1','2','3']
-
-        if opcion in valores_opcion:
-            if opcion == '1':
-                print(saludo)
-            elif opcion == '2':
-                print (f'buenos dias {nombre_usuario}')
-            elif opcion == '3':
-                numero= input('ingrese el numero que quiere (debe ser un numero MENOR A 10!): ')
-                if numero < 10:
-                    resultado = pass 
-                else:
-                    print('numero ingresado no corresponde, intentelo de nuevo.')
-
-            elif opcion == '0':
-                print('Saliendo del sistema...')
-                break
-        else:
-            print('Opción ingresada NO corresponde...')
-
-menu_principal()
