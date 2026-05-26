5002a1a515ce140d84bea8730464cacbeafe6177
Lilyrain07 <ruthelisa0717@gmail.com>
2026-05-26 10:12:35 -0400

    26/05

---
+++ modulo.py/presentacion/control_libros.py
@@ -0,0 +1,11 @@
+def agregar_libro():
+    print('agregar libro')
+
+def listado_libros():
+    print('listado de libros')
+
+def modificar_libro():
+    print('modificar libro')
+
+def eliminar_libro():
+    print('eliminar libro')
