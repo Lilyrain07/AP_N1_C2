3bd15d365fb17b7d7ce21fef2c7f0a6f02c4aeb1
Lilyrain07 <ruthelisa0717@gmail.com>
2026-05-27 09:15:31 -0400

    27/05

--- modulo.py/presentacion/control_libros.py
+++ modulo.py/presentacion/control_libros.py
@@ -1,35 +1,35 @@
 from datos import listado_libros
+from prettytable import PrettyTable
+
 
 def agregar_libro():
-    titulo = 'agregar libro'
-    print (titulo)
-    print ('=' * len(titulo))
+    titulo = 'Agregar Libro'
+    print(titulo)
+    print('=' * len(titulo))
     print('Ingrese los datos del libro:')
-    titulo_libro, isbn, editorial, paginas, categoria = solicitar_datos_libro()
-
+    titulo_libro,isbn,editorial,paginas,categoria = solicitar_datos_libro()
 
 def listar_libros():
-     titulo= 'listado de libros'
-print (titulo)
-print ('=' * len(titulo))
-print(listado_libros)
-for libro in listado_libros:
-    print(libro)
+    titulo = 'Listado de Libros'
+    print(titulo)
+    print('=' * len(titulo))
+    for libro in listado_libros:
+        print(libro)
 
 def modificar_libro():
-    titulo= 'modificar libro'
-print (titulo)
-print ('=' * len(titulo))
+    titulo = 'Modificar Libro'
+    print(titulo)
+    print('=' * len(titulo))
 
 def eliminar_libro():
-    titulo= 'eliminar libro'
-print (titulo)
-print ('=' * len(titulo))
+    titulo = 'Eliminar Libro'
+    print(titulo)
+    print('=' * len(titulo))
 
 def solicitar_datos_libro():
-    titulo_libro = input ('titulo del libro: ')
-    isbn = input ('isbn del libro: ')
-    editorial = input ('editorial del libro: ')
-    paginas = input ('cantidad de paginas del libro: ')
-    categoria = input ('categoria del libro: ')
+    titulo_libro = input('Título: ')
+    isbn = input('ISBN: ')
+    editorial = input('Editorial: ')
+    paginas = input('Cantidad de Páginas: ')
+    categoria = input('Categoría: ')
     return titulo_libro,isbn,editorial,paginas,categoria
